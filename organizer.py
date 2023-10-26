436eb1900de1ba6166efc1a7fb79b1f099a9de63
Nước Mắm Code <nathan.ng.0808@gmail.com>
2023-10-26 14:04:15 -0500

    Create organizer.py

---
+++ organizer.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+
+# If you want to have different folder for every type of files then
+# this is not the right script. I'll update a script for that later.
+
+# I only list all the files that I often download and their folders
+# they are associated with
+
+# Depends on what files you download you can add on to this list
+
+# Get types of the files
+folderName = {
+    "Compressed": {'7z','zip', 'rar'},
+    "Programming": {'py', 'js', 'html', 'css', 'java', 'c', 'cpp', 'sql'},
+    "Documents": {'pdf', 'doc', 'docx'},
+    "Images": {'jpg', 'png', 'gif', 'svg', 'bmg'},
+    "Others": {'NONE'}
+}
+
+# Extract list
+
+# I am on windows so I use '/'. If you're on Linux or macOS, please use '\'
+folderPath = r"C:/Users/natha/Downloads"
+
